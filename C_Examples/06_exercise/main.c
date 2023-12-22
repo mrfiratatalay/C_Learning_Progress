35e119742407d6415f8b92ada8a4a3945c531901
Firat Atalay <atafiratbey@gmail.com>
2023-12-22 15:45:32 +0300

    To Start to Learn C Programming Language

---
+++ C_Examples/06_exercise/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	printf("How many car you have ?\n");
+	printf("\n------------------\n");
+	printf("A-) Just only one \n");
+	printf("B-) I have 13");
+
+	printf("\n------------------\n");
+
+	return 0;
+}
