8833fdb341500f843b58c245fc204b52ccd14670
Firat Atalay <atafiratbey@gmail.com>
2023-12-22 16:12:24 +0300

    To Start to Learn C Programming Language

---
+++ C_Examples/10_exercise/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	printf("%d\n", 127);
+	printf("%f\n", 3.141593);
+	printf("%d\n", 122013);
+	printf("%f\n", 0.000080);
+	return 0;
+}
+
+/* 
+
+127
+3.141593
+122013
+0.000080
+
+*/
