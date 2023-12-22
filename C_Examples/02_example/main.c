5e7f8e400c1423723e32f0040887ead0a52c3688
Firat Atalay <atafiratbey@gmail.com>
2023-12-22 20:41:57 +0300

    To Start to Learn C Programming Language

--- C_Examples/02_example/main.c
+++ C_Examples/02_example/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
+	printf("4 times 5 is 20\n");
 	printf("4 times 5 is %d \n", 4 * 5);
 	return 0;
 }
---
+++ C_Examples/14_exercise/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("The result is %d\n", 8 * 14 * 25);
+    return(0);
+}
