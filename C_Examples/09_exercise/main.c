87debc6a4d92e06c2c0d9de5bcdef2cea3fe45a5
Firat Atalay <atafiratbey@gmail.com>
2023-12-22 16:02:20 +0300

    To Start to Learn C Programming Language

---
+++ C_Examples/09_exercise/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+
+	printf("The value %d is an integer.", 986);
+	puts("");
+	printf("The value %f is a float", 98.6);
+
+}
+
+//The value 986 is an integer.
+//The value 98.600000 is a float
