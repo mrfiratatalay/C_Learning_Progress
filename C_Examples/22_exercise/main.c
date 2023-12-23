c811b7da1f3e0d1425371a49b7b7b40220090228
Firat Atalay <atafiratbey@gmail.com>
2023-12-23 10:17:00 +0300

    To Learn C

---
+++ C_Examples/22_exercise/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+	char character = 'a';
+	int integer = 1;
+	float basitOndalik = 19.000000;
+	double buyukOndalik = 20000.009000;
+
+	printf("%c\n", character);
+	printf("%d\n", integer);
+	printf("%f\n", basitOndalik);
+	printf("%f\n", buyukOndalik);
+	return 0;
+
+}
+
+/*
+OUTPUT:
+
+a
+1
+19.000000
+20000.009000
+
+*/
