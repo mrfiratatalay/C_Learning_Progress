fccf349e9e7ac94643fe0825927bf526ec8bd442
Firat Atalay <atafiratbey@gmail.com>
2023-12-24 17:51:56 +0300

    To Learn C

---
+++ C_Examples/28_exercise/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+	int firat = 13;
+	printf("My favorite number is %d \n", firat);
+	firat = 17;
+	printf("My favorite number is %d \n", firat);
+	firat = 21;
+	printf("My favorite number is %d \n", firat);
+}
