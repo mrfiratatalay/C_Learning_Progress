392e233d4f102b722646b67aa07b4b2217d59db2
Firat Atalay <atafiratbey@gmail.com>
2023-12-24 17:39:40 +0300

    To Learn C

---
+++ C_Examples/_exerciseProject1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	/*
+	
+	int x = 13;
+	int y = 13;
+	int z = 13;
+	*/
+	// OR I CAN DO IT below way!!!
+	int x = 13, y = 13, z = 13;
+	printf("x %d\ny %d\nz %d\n", x, y, z);
+	return 0;
+}
