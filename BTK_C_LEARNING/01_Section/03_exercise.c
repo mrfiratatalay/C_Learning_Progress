043dbc9647612c1848bee350530cd606f36631f2
Firat Atalay <atafiratbey@gmail.com>
2023-12-27 15:33:30 +0300

    To Learn C

---
+++ BTK_C_LEARNING/01_Section/03_exercise.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//int main() {
+//
+//	int x = 13;
+//
+//	//PART--1
+//	/*
+//	printf("%d VERSION FIRST\n", x);
+//	x += 1;
+//	printf("%d VERSION SECOND\n", x);
+//	x -= 1;
+//	printf("%d VERSION THIRD\n", x);
+//	x *= 1;
+//	printf("%d VERSION FOURTH \n ", x);
+//	x /= 1;
+//	printf("%d VERSION last \n ", x);
+//	*/
+//
+//	//PART--2
+//
+//	printf("%d \n", x);
+//	printf("%d \n", x++);
+//	printf("%d \n", x);
+//	printf("%d \n", ++x);
+//	printf("%d \n", x);
+//
+//	return 0;
+//
+//}
