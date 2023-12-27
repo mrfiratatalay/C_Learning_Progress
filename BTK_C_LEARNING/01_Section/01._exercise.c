7e94f01ed3252600a43f9d4aee95be1342be274c
Firat Atalay <atafiratbey@gmail.com>
2023-12-27 15:23:25 +0300

    To Learn C

---
+++ BTK_C_LEARNING/01_Section/01._exercise.c
@@ -0,0 +1,12 @@
+//#include <stdio.h>
+//int main() {
+//	int x = 13, y = 14, z = 15;
+//	printf("x = %d, y = %d, z= %d",
+//		x, y, z);
+//	printf("\n******************\n");
+//	x = y = z = 13;
+//	printf("x = %d, y = %d, z= %d",
+//		x, y, z);
+//	printf("\n******************\n");
+//	return 0;
+//}
---
+++ BTK_C_LEARNING/01_Section/02_exercise.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+//
+//int main() {
+//	int number_first, number_second;
+//	int toplam, fark, carpim, mod;
+//	float bolum;
+//
+//	printf("Enter to number to screen:\n");
+//	scanf_s("%d%d", &number_first, &number_second);
+//
+//	printf("\n**********\n");
+//	printf("\n**********\n");
+//
+//	toplam = number_first + number_second;
+//	fark = number_first - number_second;
+//	carpim = number_first * number_second;
+//	mod = number_first % number_second;
+//	bolum = (float)number_first / number_second;
+//
+//
+//	printf("%d TOPLAM\n", toplam);
+//	printf("%d FARK\n", fark);
+//	printf("%d CARPIM\n", carpim);
+//	printf("%d MOD\n", mod);
+//	printf("%f BOLUM\n", bolum);
+//
+//	printf("\n**********\n");
+//	printf("\n**********\n");
+//}
