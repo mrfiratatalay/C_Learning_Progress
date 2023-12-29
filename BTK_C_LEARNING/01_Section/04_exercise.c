ea85731c87f90bda9998f2fb17b2bda4be8a6702
Firat Atalay <atafiratbey@gmail.com>
2023-12-29 14:00:53 +0300

    To Learn C

--- BTK_C_LEARNING/01_Section/04_exercise.c
+++ BTK_C_LEARNING/01_Section/04_exercise.c
@@ -1,20 +1,20 @@
-#include <stdio.h>
-int main() {
-	int numberOne = 12, numberTwo = 6;
-	int dividedIntStore;
-	float dividedFloatStore;
-	double dividedDoubleStore;
-	int convertFloatToInt;
-
-	dividedIntStore = numberOne / numberTwo;
-	printf("%d \n", dividedIntStore );
-
-	dividedFloatStore = numberOne / numberTwo;
-	printf("%.20f \n", dividedFloatStore);
-
-	dividedDoubleStore = numberOne / numberTwo;
-	printf("%.20f \n", dividedDoubleStore);
-
-	convertFloatToInt = (int) numberOne / numberTwo;
-	printf("%d \n", convertFloatToInt);
-}
+//#include <stdio.h>
+//int main() {
+//	int numberOne = 12, numberTwo = 6;
+//	int dividedIntStore;
+//	float dividedFloatStore;
+//	double dividedDoubleStore;
+//	int convertFloatToInt;
+//
+//	dividedIntStore = numberOne / numberTwo;
+//	printf("%d \n", dividedIntStore );
+//
+//	dividedFloatStore = numberOne / numberTwo;
+//	printf("%.20f \n", dividedFloatStore);
+//
+//	dividedDoubleStore = numberOne / numberTwo;
+//	printf("%.20f \n", dividedDoubleStore);
+//
+//	convertFloatToInt = (int) numberOne / numberTwo;
+//	printf("%d \n", convertFloatToInt);
+//}
---
+++ BTK_C_LEARNING/01_Section/05_exercise.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//int main() {
+//
+//	int numberOne = 10;
+//	int numberTwo = 0b0001010;
+//	int numberThree = 0x23;
+//
+//	printf("%d \n", numberOne);
+//	printf("%d \n", numberTwo);
+//	printf("%d", numberThree);
+//
+//	return 0;
+//}
