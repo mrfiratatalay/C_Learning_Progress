39216a468c847865a902dda17f9813fb82fa0101
Firat Atalay <atafiratbey@gmail.com>
2024-01-02 14:42:51 +0300

    To Learn C

---
+++ C_Examples/38_exercise/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	char a, b, c, d, e, f;
+	a = 'F';
+	b = 'I';
+	c = 'R';
+	d = 'A';
+	e = 'T';
+	f = '\n';
+
+	printf("%c%c%c%c%c%c", a, b, c, d, e, f);
+	return 0;
+}
