6ac1e8099479b252c5e05e37ae981c9fd54e3cb4
Firat Atalay <atafiratbey@gmail.com>
2024-01-02 15:04:23 +0300

    I am gonna improve my C  side :)

---
+++ C_Examples/42_exercise/main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+	/*
+	Type your first name: Danny
+Please to meet you, Danny.
+	*/
+	char myName[6];
+	printf("Type your first name: ");
+	scanf("%s", myName);
+	printf("Please to meet you, %s.", myName);
+	return 0;
+
+}
